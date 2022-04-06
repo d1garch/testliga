8498876e631cd9e31efc96661219679a12b7a909
Artem Pashintsev <art.pashintsev@gmail.com>
2022-04-06 17:00:05 +0300

    First commit

---
+++ src/main/java/ru/pashintsev/testliga/controllers/TestController.java
@@ -0,0 +1,22 @@
+package ru.pashintsev.testliga.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
+        return "/test/hello";
+    }
+}
